0b90a1a63a63c0448a1bc35e5299d3f0498b0784
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-09-17 10:47:28 -0300

    heranca

--- src/A0023-heranca/A0023-heranca.ts
+++ src/A0023-heranca/A0023-heranca.ts
@@ -1,4 +1,42 @@
 
 export class Pessoa {
+    constructor(
+        public nome: string,
+        public sobrenome: string,
+        private idade: number,
+        protected cpf: string,
+    ) {}
 
+    getIdade(): number {
+        return this.idade;
+    }
+
+    getCpf(): string {
+        return this.cpf;
+    }
+
+    getNomeCompleto(): string {
+        return `${this.nome} ${this.sobrenome}`;
+    }
 }
+
+// Aluno herda de Pessoa
+export class Aluno extends Pessoa {
+    getNomeCompleto(): string {
+        return `Isso vem do Aluno: ${this.nome} ${this.sobrenome}`;
+    }
+}
+// Cliente herda de Pessoa
+export class Cliente extends Pessoa {
+    getNomeCompleto(): string {
+        return `Isso vem de Cliente: ${this.nome} ${this.sobrenome}`;
+    }
+}
+
+const pessoa01 = new Pessoa('Vinicius', 'Dilly', 28, '123.456.789-00');
+const aluno01 = new Aluno('Vinicius', 'Dilly', 28, '123.456.789-00');
+const cliente01 = new Cliente('Olga', 'Dilly', 18, '987.654.321-00');
+
+console.log(pessoa01.getNomeCompleto());
+console.log(aluno01.getNomeCompleto());
+console.log(cliente01.getNomeCompleto());
