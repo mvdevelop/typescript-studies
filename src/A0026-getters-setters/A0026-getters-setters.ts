f6ffba44d229501f40cac1d1ef2eb5afb3bfba3a
mvdevelop <marcosvmdilly@gmail.com>
2026-01-05 12:45:36 -0300

    feat: working on static.ts

--- src/A0026-getters-setters/A0026-getters-setters.ts
+++ src/A0026-getters-setters/A0026-getters-setters.ts
@@ -5,13 +5,17 @@ export class Pessoa {
         private sobrenome: string,
         private idade: number,
         private _cpf: string,
-    ) {}
+    ) {
+        this.cpf = _cpf;
+    }
 
     set cpf(cpf: string) {
+        console.log('Setter Chamado!');
         this._cpf = cpf;
     }
 
     get cpf(): string {
+        console.log('Getter Chamado!');
         return this._cpf.replace(/\D/g, '');
     }
 }
---
+++ src/A0027-static/A0027-static.ts
@@ -0,0 +1,28 @@
+
+// A0027 - static
+
+export class Pessoa {
+    static idadePadrao = 0;
+    static cpfPadrao = '000.000.000-00';
+
+    constructor(
+        public nome: string,
+        public sobrenome: string,
+        public idade: number,
+        public cpf: string,
+    ) {}
+
+    metodoNormal(): void {
+        console.log(Pessoa.idadePadrao, Pessoa.cpfPadrao);
+    }
+
+    static criaPessoa(nome: string, sobrenome: string): Pessoa {
+        return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
+    }
+}
+
+const pessoa01 = new Pessoa('Vinicius', 'Dilly', 28, '123.456.789-19');
+const pessoa02 = Pessoa.criaPessoa('Drak', 'Lians');
+
+console.log(pessoa01);
+console.log(pessoa02);
