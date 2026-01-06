241f409f76e5e8f8e815a2f8bd63b5918d475b2b
mvdevelop <marcosvmdilly@gmail.com>
2026-01-06 12:35:57 -0300

    feat: working on constructor-privado

--- src/A0028-construtor-privado/A0028-construtor-privado.ts
+++ src/A0028-construtor-privado/A0028-construtor-privado.ts
@@ -1,8 +1,11 @@
 
 // A0028-construtor-privado
 
+// Singleton (GOF): Padrão de projeto que restringe a instanciação de uma classe a um único objeto.
 export class Database {
-    constructor(
+    private static database: Database;
+
+    private constructor(
         private host: string,
         private user: string,
         private password: string,
@@ -11,7 +14,14 @@ export class Database {
     connect(): void {
         console.log(`Connecting to: ${this.host}, ${this.user}, ${this.password}`);
     }
+
+    static getDatabase(host: string, user: string, password: string): Database {}
 }
 
-const dataBase = new Database("localhost", "root", "123456");
-dataBase.connect();
+const dataBase01 = Database.getDatabase("localhost", "root", "123456");
+dataBase01.connect();
+
+const dataBase02 = Database.getDatabase("localhost", "root", "123456");
+dataBase02.connect();
+
+console.log(dataBase01 === dataBase02);
