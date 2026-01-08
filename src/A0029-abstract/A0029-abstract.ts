bc32c2c2936a022bcba15d41c6103a89b68f0331
mvdevelop <marcosvmdilly@gmail.com>
2026-01-08 13:10:31 -0300

    feat: working on abstract

--- src/A0029-abstract/A0029-abstract.ts
+++ src/A0029-abstract/A0029-abstract.ts
@@ -9,6 +9,34 @@ export class Personagem {
     ) { }
 
     atacar(personagem: Personagem): void {
-        
+        this.bordao();
+        personagem.perderVida(this.ataque);
+    }
+
+    perderVida(forcaAtaque: number): void {
+        this.vida -= forcaAtaque;
+        console.log(`${this.nome} agora tem ${this.vida - forcaAtaque} de vida.`);
+    }
+
+    bordao(): void {
+        console.log("Força e honra!");
     }
 }
+
+export class Guerreira extends Personagem {
+    bordao(): void {
+        console.log("Força e honra!");
+    }
+};
+
+export class Monstro extends Personagem {};
+
+const guerreira = new Guerreira("Xena", 100, 1000);
+const monstro = new Monstro("Hidra", 80, 2000);
+
+guerreira.atacar(monstro);
+monstro.atacar(guerreira);
+guerreira.atacar(monstro);
+monstro.atacar(guerreira);
+guerreira.atacar(monstro);
+monstro.atacar(guerreira);
