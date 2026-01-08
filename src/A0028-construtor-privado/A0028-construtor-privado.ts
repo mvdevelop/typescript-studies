8ffa25a777c93251f55e5b3455a54260b6d96c6b
mvdevelop <marcosvmdilly@gmail.com>
2026-01-08 12:29:28 -0300

    feat: working on abstract

--- src/A0028-construtor-privado/A0028-construtor-privado.ts
+++ src/A0028-construtor-privado/A0028-construtor-privado.ts
@@ -2,6 +2,7 @@
 // A0028-construtor-privado
 
 // Singleton (GOF): Padrão de projeto que restringe a instanciação de uma classe a um único objeto.
+// Factory Method (GOF): Padrão de projeto que define uma interface para criar um objeto, mas permite que as subclasses decidam qual classe instanciar.
 export class Database {
     private static database: Database;
 
@@ -15,7 +16,16 @@ export class Database {
         console.log(`Connecting to: ${this.host}, ${this.user}, ${this.password}`);
     }
 
-    static getDatabase(host: string, user: string, password: string): Database {}
+    static getDatabase(host: string, user: string, password: string): Database {
+        if (Database.database) {
+            console.log("Retornando instância já criada.");
+            return Database.database;
+        }
+        console.log("Criando nova instância.");
+
+        Database.database = new Database(host, user, password);
+        return Database.database;
+    }
 }
 
 const dataBase01 = Database.getDatabase("localhost", "root", "123456");
@@ -24,4 +34,8 @@ dataBase01.connect();
 const dataBase02 = Database.getDatabase("localhost", "root", "123456");
 dataBase02.connect();
 
-console.log(dataBase01 === dataBase02);
+const dataBase03 = Database.getDatabase("localhost", "root", "123456");
+dataBase03.connect();
+
+const dataBase04 = Database.getDatabase("localhost", "root", "123456");
+dataBase04.connect();
---
+++ src/A0029-abstract/A0029-abstract.ts
@@ -0,0 +1,14 @@
+
+// A0029-abstract
+
+export class Personagem {
+    constructor(
+        protected nome: string,
+        protected ataque: number,
+        protected vida: number,
+    ) { }
+
+    atacar(personagem: Personagem): void {
+        
+    }
+}
