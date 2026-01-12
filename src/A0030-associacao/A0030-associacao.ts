815989f2d86f9c9abf3c4c6f826f5193fedfe31e
mvdevelop <marcosvmdilly@gmail.com>
2026-01-12 12:30:44 -0300

    feat: working on associacao

--- src/A0030-associacao/A0030-associacao.ts
+++ src/A0030-associacao/A0030-associacao.ts
@@ -5,6 +5,27 @@ export class Escritor {
     private _ferramenta: Ferramenta | null = null;
 
     constructor(private _nome: string) {}
+
+    get nome(): string {
+        return this._nome;
+    }
+
+    set ferramenta(ferramenta: Ferramenta | null) {
+        this._ferramenta = ferramenta;
+    }
+
+    get ferramenta(): Ferramenta | null {
+        return this._ferramenta;
+    }
+
+    escrever(): void {
+        if (this.ferramenta === null) {
+            console.log(`${this._nome} não tem uma ferramenta para escrever.`);
+            return;
+        }
+
+        this.ferramenta.escrever();
+    }
 }
 
 export abstract class Ferramenta {
@@ -15,3 +36,26 @@ export abstract class Ferramenta {
         return this._nome;
     }
 }
+
+export class Caneta extends Ferramenta {
+    escrever(): void {
+        console.log(`Escrevendo com a caneta ${this.nome}`);
+    }
+}
+
+export class MaquinaDeEscrever extends Ferramenta {
+    escrever(): void {
+        console.log(`Digitando com a máquina de escrever ${this.nome}`);
+    }
+}
+
+const escritor = new Escritor('Vinicius');
+const caneta = new Caneta('Bic Cristal');
+const maquinaDeEscrever = new MaquinaDeEscrever('IBM Model M');
+
+// console.log(escritor.nome);
+// console.log(caneta.nome);
+// console.log(maquinaDeEscrever.nome);
+
+escritor.ferramenta = caneta;
+escritor.escrever();
