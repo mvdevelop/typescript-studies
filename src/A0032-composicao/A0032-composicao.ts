afa881a44566a3d9eb6e0e3d9aec3ae12423f9b3
mvdevelop <marcosvmdilly@gmail.com>
2026-01-14 10:52:50 -0300

    feat: working on composicao

--- src/A0032-composicao/A0032-composicao.ts
+++ src/A0032-composicao/A0032-composicao.ts
@@ -1,1 +1,27 @@
 
+// A0032-composicao.ts
+
+export class Carro {
+    public readonly motor = new Motor();
+}
+
+export class Motor {
+    ligar(): void {
+        console.log('Motor está ligado...');
+    }
+
+    acelerar(): void {
+        console.log('Motor está acelerando...');
+    }
+
+    frear(): void {
+        console.log('Motor está freando...');
+    }
+
+    desligar(): void {
+        console.log('Motor está desligado...');
+    }
+}
+
+const carro = new Carro();
+carro.motor.ligar();
