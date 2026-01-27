7223f7ae94f28008ac40bb788dc63fc510931c17
mvdevelop <marcosvmdilly@gmail.com>
2026-01-27 14:50:33 -0300

    feat: working on exercicio-video

--- src/A0035-exercicio-video/A0035-exercicio-video.ts
+++ src/A0035-exercicio-video/A0035-exercicio-video.ts
@@ -1,5 +1,5 @@
 
-// console.log('Hello');
+// A0035-exercicio-video.ts
 
 interface VideoPlayerElements {
     videoPlayer: HTMLVideoElement;
@@ -9,4 +9,30 @@ interface VideoPlayerElements {
 
 interface VideoPlayerProtocol {
     playToggle(): void;
+    stop(): void;
+    iniciarEventos(): void;
 }
+
+export default class VideoPlayer implements VideoPlayerProtocol {
+    private videoPlayer: HTMLVideoElement;
+    private playButton: HTMLButtonElement;
+    private stopButton: HTMLButtonElement;
+
+    constructor(videoPlayerElements: VideoPlayerElements) {
+        this.videoPlayer = videoPlayerElements.videoPlayer;
+        this.playButton = videoPlayerElements.playButton;
+        this.stopButton = videoPlayerElements.stopButton;
+    }
+
+    iniciarEventos(): void { }
+
+    playToggle(): void { }
+
+    stop(): void { }
+}
+
+const videoPlayer = new VideoPlayer({
+    videoPlayer: document.querySelector('.video') as HTML,
+    playButton: HTMLButtonElement;
+    stopButton: HTMLButtonElement;
+});
