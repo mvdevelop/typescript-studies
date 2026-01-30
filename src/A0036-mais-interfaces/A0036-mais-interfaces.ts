a98fa6209d1fe7ff98e6b143ee925229ac54d64d
mvdevelop <marcosvmdilly@gmail.com>
2026-01-29 21:59:29 -0300

    feat: working on A0036

--- src/A0036-mais-interfaces/A0036-mais-interfaces.ts
+++ src/A0036-mais-interfaces/A0036-mais-interfaces.ts
@@ -1,1 +1,20 @@
 
+interface Pessoa {
+    nome: string;
+};
+
+interface Pessoa {
+    readonly sobrenome: string;
+}
+
+interface Pessoa {
+    readonly enderecos: string[];
+}
+
+const pessoa: Pessoa = {
+    nome: "Vinicius",
+    sobrenome: "Marinho",
+};
+
+//pessoa.sobrenome = "";
+console.log(pessoa);
