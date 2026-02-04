2525885b1f497146ccc61671d5c0f57fa0de582b
mvdevelop <marcosvmdilly@gmail.com>
2026-02-03 21:15:26 -0300

    feat: working on keyof typeof

--- src/A0038-keyof-typeof/A0038-keyof-typeof.ts
+++ src/A0038-keyof-typeof/A0038-keyof-typeof.ts
@@ -1,19 +1,29 @@
 
 // A0038-keyof-typeof.ts
 
-type CoresObj = {
-    vermelho: string;
-    azul: string;
-    verde: string;
+// type CoresObj = {
+//     vermelho: string;
+//     azul: string;
+//     verde: string;
+// };
 
-};
+type CoresObj = typeof coresObj;
 
-const coresObj: CoresObj = {
+type CoresChaves = keyof CoresObj;
+
+const coresObj = {
     vermelho: 'red',
     azul: 'blue',
     verde: 'green',
 };
 
-function traduzirCor(cor: string, cores: CoresObj) {
+function traduzirCor(
+    cor: 'vermelho' | 'verde' | 'azul',
+    cores: CoresObj,
+) {
     return cores[cor];
 }
+
+console.log(traduzirCor('vermelho', coresObj));
+console.log(traduzirCor('azul', coresObj));
+console.log(traduzirCor('verde', coresObj));
