852265134c565eed256fc65b261bb6da89c995a8
mvdevelop <marcosvmdilly@gmail.com>
2026-02-05 15:35:31 -0300

    feat: working on A0041-overload-funcoes

--- src/A0041-overload-funcoes/A0041-overload-funcoes.ts
+++ src/A0041-overload-funcoes/A0041-overload-funcoes.ts
@@ -1,1 +1,15 @@
 
+// A0041-overload-funcoes.ts
+
+type Adder = {
+    (x: number): number;
+    (x: number, y: number): number;
+    (x: string): string;
+    (x: boolean): boolean;
+    (...args: number[]): number;
+};
+
+const adder: Adder = (x: number, y?: number, ...args: number[]): any => {
+    if (args.length > 0) return args.reduce((s, val) => s + val, 0) + x + (y || 0);
+    return y !== undefined ? x + y : x;
+};
