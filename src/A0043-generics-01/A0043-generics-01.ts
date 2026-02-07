4b5a0509e574b656ebdc3fe6bf15138e8d029c9f
mvdevelop <marcosvmdilly@gmail.com>
2026-02-07 12:39:28 -0300

    feat: working on generics-01

--- src/A0043-generics-01/A0043-generics-01.ts
+++ src/A0043-generics-01/A0043-generics-01.ts
@@ -1,24 +1,26 @@
 
 // A0043-generics-01.ts
 
-type FilterCallback = (value: unknown, index?: number, array?: unknown[]) => boolean;
+type FilterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
 
-function meuFilter(
-    array: unknown[],
-    callbackfn: FilterCallback) {
-        const novoArray = [];
+export function meuFilter<T>(
+    array: T[],
+    callbackfn: FilterCallback<T>): T[] {
+    const novoArray = [];
 
-        for (let i = 0; i < array.length; i++) {
-            if (callbackfn(array[i])) {
-                novoArray.push(array[i]);
-            }
+    for (let i = 0; i < array.length; i++) {
+        if (callbackfn(array[i])) {
+            novoArray.push(array[i]);
         }
-
-        return novoArray;
     }
+
+    return novoArray;
+}
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const arrayFiltradoOriginal = array.filter((value) => value < 5);
-
 console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = meuFilter(array, (value) => value < 5);
+console.log(arrayFiltrado);
