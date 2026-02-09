bb03dbcf46f37702e5a96bd817211679543d9537
mvdevelop <marcosvmdilly@gmail.com>
2026-02-09 19:01:36 -0300

    feat: working on generics-03

--- src/A0045-generics-03/A0045-generics-03.ts
+++ src/A0045-generics-03/A0045-generics-03.ts
@@ -1,8 +1,28 @@
 
 // A0045-generics-03.ts
 
-interface PessoaProtocolo<T, U> {
+interface PessoaProtocolo<T = string, U = number> {
     nome: T;
     sobrenome: T;
     idade: U;
 }
+
+const aluno01: PessoaProtocolo<string, number> = {
+    nome: "Vinicius",
+    sobrenome: "Marinho",
+    idade: 28,
+};
+
+const aluno02: PessoaProtocolo<string, number> = {
+    nome: "Drak",
+    sobrenome: "Lians",
+    idade: 28,
+};
+
+const aluno03: PessoaProtocolo = {
+    nome: "Drak",
+    sobrenome: "Lians",
+    idade: 28,
+};
+
+console.log(aluno01, aluno02, aluno03);
