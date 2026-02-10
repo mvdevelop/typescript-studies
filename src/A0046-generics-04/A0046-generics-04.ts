d9c9224555a2ec0c128254c43ab47b1b8225ca9c
mvdevelop <marcosvmdilly@gmail.com>
2026-02-10 12:38:09 -0300

    feat: working on generics-04

--- src/A0046-generics-04/A0046-generics-04.ts
+++ src/A0046-generics-04/A0046-generics-04.ts
@@ -3,4 +3,17 @@
 
 type ObterChaveFn = <O, K extends keyof O>(obj: O, chave: K) => O[K];
 
+const obterChave: ObterChaveFn = (obj, chave) => obj[chave];
+
+const animal = {
+    nome: 'Cachorro',
+    idade: 5,
+    raca: 'Labrador',
+    vacinas: ['V8', 'V10', 'Raiva'],
 };
+
+const nomeAnimal = obterChave(animal, 'nome');
+const vacinas = obterChave(animal, 'vacinas');
+
+console.log(nomeAnimal);
+console.log(vacinas);
