f8a76aa475629ed2d3089c5cf7cb5bf55f52633f
mvdevelop <marcosvmdilly@gmail.com>
2026-02-11 15:59:21 -0300

    feat: working on generics-05

--- src/A0047-generics-05/A0047-generics-05.ts
+++ src/A0047-generics-05/A0047-generics-05.ts
@@ -5,11 +5,15 @@ export class Pessoa<T, U> {
     constructor(public nome: T, public idade: U) {}
 }
 
-const pessoa01 = new Pessoa<string, number>('Vinicius', 26);
-const pessoa02 = new Pessoa<string, number>('Maria', 30);
+const pessoa01 = new Pessoa('Vinicius', 26);
+const pessoa02 = new Pessoa(['Maria'], 48);
+const pessoa03 = new Pessoa(['Marcos'], { idade : 32 });
+const pessoa04 = new Pessoa(['Marcos'], { idade : 32 });
 
 console.log(pessoa01.nome);
 console.log(pessoa01.idade);
 
 console.log(pessoa02.nome);
 console.log(pessoa02.idade);
+
+console.log(pessoa03);
