fca847f60808a93ef4aba7d23bc76da935c24a51
mvdevelop <marcosvmdilly@gmail.com>
2026-02-15 18:40:38 -0300

    feat: working on A0051-decorator-class

---
+++ src/A0051-decorator-class/A0051-decorator-class.ts
@@ -0,0 +1,11 @@
+
+// A0051-decorator-class
+
+export class Animal {
+    constructor(public cor: string) {
+
+    }
+}
+
+const animal = new Animal('roxo');
+console.log(animal);
--- src/A0059-exercicio-votacao/A0059-exercicio-votacao.ts
+++ src/A0059-exercicio-votacao/A0059-exercicio-votacao.ts
@@ -48,8 +48,28 @@ const votation01 = new Votation('Qual é sua linguagem de programação favorita
 votation01.addVotationOption({ option: 'Python', numberOfvotes: 0 });
 votation01.addVotationOption({ option: 'JavaScript', numberOfvotes: 0 });
 votation01.addVotationOption({ option: 'Java', numberOfvotes: 0 });
+votation01.vote(1);
+votation01.vote(1);
+votation01.vote(0);
+votation01.vote(0);
+votation01.vote(0);
+votation01.vote(2);
+
+const votation02 = new Votation('Qual é sua cor favorita ?');
+votation02.addVotationOption({ option: 'Azul', numberOfvotes: 0 });
+votation02.addVotationOption({ option: 'Vermelho', numberOfvotes: 0 });
+votation02.addVotationOption({ option: 'Verde', numberOfvotes: 0 });
+votation02.addVotationOption({ option: 'Roxo', numberOfvotes: 0 });
+votation02.addVotationOption({ option: 'Amarelo', numberOfvotes: 0 });
+votation02.vote(0);
+votation02.vote(2);
+votation02.vote(2);
+votation02.vote(1);
+votation02.vote(0);
+votation02.vote(2);
 
 const votationApp = new VotationApp();
 votationApp.addVotation(votation01);
+votationApp.addVotation(votation02);
 
 votationApp.showVotations();
