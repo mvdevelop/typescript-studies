78b24bff832393efaf87c687720c9a9f917a67e3
mvdevelop <marcosvmdilly@gmail.com>
2026-02-15 12:00:12 -0300

    feat: working on exercicio-votacao

--- src/A0059-exercicio-votacao/A0059-exercicio-votacao.ts
+++ src/A0059-exercicio-votacao/A0059-exercicio-votacao.ts
@@ -18,6 +18,10 @@ export class Votation {
         if (!this._votationOptions[votationIndex]) return;
         this._votationOptions[votationIndex].numberOfvotes += 1;
     }
+
+    get votationOption(): VotationOption[] {
+        return this._votationOptions;
+    }
 };
 
 export class VotationApp {
@@ -26,4 +30,26 @@ export class VotationApp {
     addVotation(votation: Votation): void {
         this.votations.push(votation);
     }
+
+    showVotations(): void {
+        for (const votation of this.votations) {
+            console.log(votation.details);
+            for (const votationOption of votation.votationOption) {
+                console.log(votationOption.option, votationOption.numberOfvotes);
+            }
+
+            console.log('###');
+            console.log('');
+        }
+    }
 }
+
+const votation01 = new Votation('Qual é sua linguagem de programação favorita ?');
+votation01.addVotationOption({ option: 'Python', numberOfvotes: 0 });
+votation01.addVotationOption({ option: 'JavaScript', numberOfvotes: 0 });
+votation01.addVotationOption({ option: 'Java', numberOfvotes: 0 });
+
+const votationApp = new VotationApp();
+votationApp.addVotation(votation01);
+
+votationApp.showVotations();
