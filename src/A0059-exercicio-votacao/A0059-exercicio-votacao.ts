83bcb53c06e80b70af516bd7d9c2cb7e4d0874d6
mvdevelop <marcosvmdilly@gmail.com>
2026-02-15 11:16:25 -0300

    feat: working on exercicio-votacao

--- src/A0059-exercicio-votacao/A0059-exercicio-votacao.ts
+++ src/A0059-exercicio-votacao/A0059-exercicio-votacao.ts
@@ -7,5 +7,23 @@ type VotationOption = {
 };
 
 export class Votation {
+    private _votationOptions: VotationOption[] = [];
+    constructor(public details: string) {}
 
+    addVotationOption(votationOption: VotationOption): void {
+        this._votationOptions.push(votationOption);
+    }
+
+    vote(votationIndex: number): void {
+        if (!this._votationOptions[votationIndex]) return;
+        this._votationOptions[votationIndex].numberOfvotes += 1;
+    }
+};
+
+export class VotationApp {
+    private votations: Votation[] = [];
+
+    addVotation(votation: Votation): void {
+        this.votations.push(votation);
+    }
 }
