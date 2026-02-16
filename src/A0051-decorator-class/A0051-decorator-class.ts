42936f0cace6963e1c48380f50dca8b1ed67972a
mvdevelop <marcosvmdilly@gmail.com>
2026-02-16 17:28:37 -0300

    working on decorator-class-02

--- src/A0051-decorator-class/A0051-decorator-class.ts
+++ src/A0051-decorator-class/A0051-decorator-class.ts
@@ -1,24 +1,29 @@
 
 // A0051-decorator-class
 
+@decorator
 export class Animal {
-    constructor(public cor: string) {
-
-    }
+    constructor(public nome: string, public cor: string) {}
 }
 
 function decorator<T extends new (...args: any[]) => any>(target: T): T {
     return class extends target {
         cor: string;
+        nome: string;
 
         constructor(...args: any[]) {
             super(...args);
-            this.cor = args[0].split('').reverse();
+            this.nome = this.inverte(args[0]);
+            this.cor = this.inverte(args[1]);
+        }
+
+        inverte(valor: string): string {
+            return valor.split('').reverse().join('');
         }
     };
 }
 
-const AnimalDecorated = decorator(Animal);
-const animal = new AnimalDecorated('roxo');
+//const AnimalDecorated = decorator(Animal);
+const animal = new Animal('Drak', 'roxo');
 
 console.log(animal);
