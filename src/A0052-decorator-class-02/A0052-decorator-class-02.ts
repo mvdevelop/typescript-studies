7fff0e69a325ec1d174bbe2a7d913b7000a18fbb
mvdevelop <marcosvmdilly@gmail.com>
2026-02-16 18:10:28 -0300

    working on decorator-class-03

--- src/A0052-decorator-class-02/A0052-decorator-class-02.ts
+++ src/A0052-decorator-class-02/A0052-decorator-class-02.ts
@@ -1,26 +1,49 @@
 
 // A0051-decorator-class
 
+interface Constructor<T = {}> {
+    new (...args: any[]): T;
+}
+
 function inverteNomeECor(param01: string, param02: string) {
-    return function inverteNomeECor<T extends new (...args: any[]) => any>(target: T): T {
-    console.log('Decorador inverteNomeECor aplicado à classe:', target.name);
+    // Closure para armazenar os parâmetros do decorador
+    return function <T extends Constructor<{ nome: string; cor: string }>>(
+        target: T
+    ): T {
+        console.log(
+            'Decorador inverteNomeECor aplicado à classe:',
+            target.name
+        );
 
-    return class extends target {
-        cor: string;
-        nome: string;
+        return class extends target {
+            constructor(...args: any[]) {
+                super(...args);
 
-        constructor(...args: any[]) {
-            super(...args);
-            this.nome = this.inverte(args[0]);
-            this.cor = this.inverte(args[1]);
-        }
+                this.nome = this.inverte(this.nome);
+                this.cor = this.inverte(this.cor);
+            }
 
-        inverte(valor: string): string {
-            return valor.split('').reverse().join('');
-        }
+            private inverte(valor: string): string {
+                return (
+                    valor.split('').reverse().join('') +
+                    ` (${param01}, ${param02})`
+                );
+            }
+        } as T; // 👈 necessário para manter compatibilidade do tipo
     };
 }
 
+function outroDecorador(param01: string, param02: string) {
+    return function <T extends Constructor>(target: T): T {
+        console.log(
+            'Outro decorador aplicado à classe:',
+            target.name
+        );
+        return target;
+    };
+}
+
+@outroDecorador('outroValor01', 'outroValor02')
 @inverteNomeECor('valor01', 'valor02')
 export class Animal {
     constructor(public nome: string, public cor: string) {
