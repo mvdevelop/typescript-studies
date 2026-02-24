d087d3d6ec9f65df857c2cd6f4b0d45800d436fb
mvdevelop <marcosvmdilly@gmail.com>
2026-02-23 21:50:02 -0300

    feat: working on A0058-decorators

---
+++ src/A0060-namespaces/index.ts
@@ -0,0 +1,9 @@
+
+namespace MeuNamespace {
+    //console.log("Olá, eu sou um namespace!");
+    const nome = "Vinicius";
+
+    class PessoaDoNamespace {
+        constructor(public nome: string) { }
+    }
+}
