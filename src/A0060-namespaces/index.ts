d60d20cc43631453892e15f34094c6fba5f79261
mvdevelop <marcosvmdilly@gmail.com>
2026-02-24 21:29:29 -0300

    feat: working on namespaces

--- src/A0060-namespaces/index.ts
+++ src/A0060-namespaces/index.ts
@@ -1,12 +1,14 @@
 
 namespace MeuNamespace {
-    //console.log("Olá, eu sou um namespace!");
     const nome = "Vinicius";
 
-    class PessoaDoNamespace {
+    export class PessoaDoNamespace {
         constructor(public nome: string) { }
     }
 
-    const pessoa = new PessoaDoNamespace(nome);
-    console.log(pessoa.nome);
+    const pessoa = new PessoaDoNamespace('Vinicius');
+    console.log(pessoa);
 }
+
+const pessoa = new PessoaDoNamespace('Marcos');
+console.log(pessoa.nome);
