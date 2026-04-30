9a93ca2647a5287781dac1da5e01a1eba6997e86
mvdevelop <marcosvmdilly@gmail.com>
2026-04-30 10:05:47 -0300

    feat: working on namespaces

--- src/A0060-namespaces/index.ts
+++ src/A0060-namespaces/index.ts
@@ -1,22 +1,4 @@
 
-namespace MeuNamespace {
-    export const nomeDoNamespace = "Marcos";
+/// <reference path="module.ts" />
 
-    export class PessoaDoNamespace {
-        constructor(public nome: string) { }
-    }
-
-    const pessoaDoNamespace = new PessoaDoNamespace('Marcos');
-    console.log(pessoaDoNamespace);
-
-    export namespace OutroNamespace {
-        export const nomeDoOutroNamespace = "Camila";
-    }
-}
-
-const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Vinicius');
-console.log(pessoaDoNamespace);
 console.log(MeuNamespace.nomeDoNamespace);
-console.log(MeuNamespace.OutroNamespace.nomeDoOutroNamespace);
-
-export default 1;
--- src/A0060-namespaces/module.ts
+++ src/A0060-namespaces/module.ts
@@ -1,1 +1,22 @@
 
+namespace MeuNamespace {
+    export const nomeDoNamespace = "Marcos";
+
+    export class PessoaDoNamespace {
+        constructor(public nome: string) { }
+    }
+
+    const pessoaDoNamespace = new PessoaDoNamespace('Marcos');
+    console.log(pessoaDoNamespace);
+
+    export namespace OutroNamespace {
+        export const nomeDoOutroNamespace = "Camila";
+    }
+}
+
+const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Vinicius');
+console.log(pessoaDoNamespace);
+console.log(MeuNamespace.nomeDoNamespace);
+console.log(MeuNamespace.OutroNamespace.nomeDoOutroNamespace);
+
+//export default 1;
