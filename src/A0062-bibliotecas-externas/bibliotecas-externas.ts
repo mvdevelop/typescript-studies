9a37c966377c725caa919984e3ca0c69eb1a3f50
mvdevelop <marcosvmdilly@gmail.com>
2026-05-04 11:30:52 -0300

    feat: working on A0063

--- src/A0062-bibliotecas-externas/bibliotecas-externas.ts
+++ src/A0062-bibliotecas-externas/bibliotecas-externas.ts
@@ -3,4 +3,4 @@ import validator from "validator";
 import _ from "lodash";
 
 console.log(validator.isEmail("test@example.com"));
-console.log(_.upperFirst("hello world"));
+console.log(_.clone("Hello, world!"));
